50ece1f0b91424c810a235722b838cac0d275882
Kristian Lorbetzki <kristian@lorbetzki.net>
2023-08-18 23:07:31 +0200

    add german language

--- libs/datapoints.php
+++ libs/datapoints.php
@@ -6,11 +6,11 @@ $DP = [
       ['temp-bed',				    $this->Translate('Temperature bed'),			'FLOAT', 'PRLL_Temp', 10          ],
       ['temp-nozzle',			    $this->Translate('Temperature nozzle'),		'FLOAT', 'PRLL_Temp', 10          ],
       ['material',				    $this->Translate('loaded material'),			'STRING', '', 20                  ],
-      ['state',					      $this->Translate('Printer status'),				'STRING', '', 20                  ],
+      ['state',					      $this->Translate('printer status'),				'STRING', '', 20                  ],
       ['estimatedPrintTime',	$this->Translate('estimated printtime'),	'INT', '~UnixTimestampTime', 30  ],
       ['name',					      $this->Translate('filename'),						  'STRING', '', 20                 ],
       ['completion',		    	$this->Translate('progress'),						  'FLOAT', '~Progress', 20         ],
       ['printTime',			    	$this->Translate('print time'),						'INT', '~UnixTimestampTime', 30  ],
       ['printTimeLeft',		   	$this->Translate('print time left'),			'INT', '~UnixTimestampTime', 30  ],
-      ['printTimeReady',		  $this->Translate('Printing expected to be ready'),			'INT', '~UnixTimestamp', 30  ]
+      ['printTimeReady',		  $this->Translate('printing expected to be ready'),			'INT', '~UnixTimestamp', 30  ]
 ];
